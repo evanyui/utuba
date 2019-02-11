954eaffb5ee39f02868366a5c8144a7ce4d1d575
Chuanyui Teh <evanyui@hotmail.com>
2019-02-11 00:25:59 -0800

    Minimum setup with webpack, react, and electron

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import '../assets/css/App.css'
+import React, { Component } from 'react'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, Utuba!</h1>
+        <h2>Coming Soon</h2>
+        <p>Utuba in development.</p>
+      </div>
+    )
+  }
+}
+
+export default App
